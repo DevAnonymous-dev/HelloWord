8d510b178eadb5e5775d05525796948734d54a0a
hibaSbei <hiba.enis@gmail.com>
2014-12-07 01:05:51 +0100

    Second commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+public static void main(String args[]){
+Somme S =new Somme();
+Helloword hello=new Helloword();
+hello.affichage();
+System.out.println("la somme egale a "+S.somme(3,3);
+}
+}
