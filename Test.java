f54c4767cefc836cb47a34ec191d93968e4a33f4
hibaSbei <hiba.enis@gmail.com>
2014-12-09 23:14:18 +0100

    teste

--- Test.java
+++ Test.java
@@ -2,8 +2,9 @@ public class Test{
 public static void main(String args[]){
 Somme S =new Somme();
 Helloword hello=new Helloword();
-hello.affichage()
+hello.affichager();
 hello.affichage();
 System.out.println("la somme egale a "+S.somme(jp,3);
+
 }
 }
