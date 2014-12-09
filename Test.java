8eb755a294162b03c0cf740d7448fd72e7d8dcad
hibaSbei <hiba.enis@gmail.com>
2014-12-09 22:45:11 +0100

    Ess1

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ public class Test{
 public static void main(String args[]){
 Somme S =new Somme();
 Helloword hello=new Helloword();
+hello.affichage()
 hello.affichage();
-System.out.println("la somme egale a "+S.somme(S,3);
+System.out.println("la somme egale a "+S.somme(2,3);
 }
 }
